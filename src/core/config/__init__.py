09961302aeb2afda9dfe5e73d64a2dbb52745432
Maxim <93401048+Kimiyori@users.noreply.github.com>
2025-09-22 14:37:08 +0300

    Stop tracking env files and ignore them

--- src/core/config/__init__.py
+++ src/core/config/__init__.py
@@ -1,14 +1,13 @@
 from src.core.config.application import ApplicationSettings
-from src.core.config.auth import AuthSettings
 from src.core.config.db import DatabaseSettings
 
 APP_SETTINGS = ApplicationSettings()
-AUTH_SETTINGS = AuthSettings()
+# AUTH_SETTINGS = AuthSettings()
 DB_SETTINGS = DatabaseSettings()
 
 
 __all__ = [
     "APP_SETTINGS",
-    "AUTH_SETTINGS",
+    # "AUTH_SETTINGS",
     "DB_SETTINGS",
 ]
